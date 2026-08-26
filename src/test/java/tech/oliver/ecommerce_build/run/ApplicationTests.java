e209d4dfe0f2a49e9c98170fb86d027bebe2121c
OliverBR1 <Br.0liveira@outlook.com>
2026-08-26 20:36:22 -0300

    first commit

---
+++ src/test/java/tech/oliver/ecommerce_build/run/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.oliver.ecommerce_build.run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
